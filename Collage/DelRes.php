e786569550e1e040fa79fda7c10085b2512ebdd6
1abdullah-saad1 <53198430+1abdullah-saad1@users.noreply.github.com>
2021-06-23 00:32:26 +0300

    Update DelRes.php

--- Collage/DelRes.php
+++ Collage/DelRes.php
@@ -6,5 +6,15 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
-if($_SERVER[""]=="GET"){}
+if ($_SESSION["userrole"] != 2) {
+    header("location: ../");
+    exit;
+  }
+  $sql="DELETE FROM `studentforma` WHERE `id`='".$_GET["id"]."';";
+  if ($stmt = mysqli_prepare($link, $qury)) {
+    if (mysqli_stmt_execute($stmt))
+    {
+        header("location: ./");
+    }
+}
 ?>
