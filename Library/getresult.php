4dd42d4b3e0bda97c785de3e88135f753c47485f
wwwleathcom <wwwleathcom@gmail.com>
2021-06-23 01:04:52 +0300

    add library with pagination

---
+++ Library/getresult.php
@@ -0,0 +1,67 @@
+<?php
+/*
+Author : Sunil Bhatt
+*/
+require_once("dbclass.php");
+require_once("pagination.class.php");
+$dbConnection  = new Connection();
+$perPage       = new sbpagination();
+
+$sqlquery       = "SELECT * from studentforma ";
+
+$page = 1;
+if(!empty($_GET["page"])) {
+$page = $_GET["page"];
+}
+
+$start = ($page-1)*$perPage->perpage;
+if($start < 0) $start = 0;
+
+$query   =  $sqlquery . " limit " . $start . "," . $perPage->perpage; 
+$getData = $dbConnection->runQuery($query);
+
+$rowcount      = $dbConnection->numRows($sqlquery);
+$showpagination = $perPage->getAllPageLinks($rowcount);	
+
+$output = '';
+$output .= ' <table class="table table-bordered">
+<thead>                  
+<tr class="bg-dark">
+<th class="text-center" >الكلية</th>
+<th class="text-center" >اسم الطالب</th>
+<th class="text-center" >المشرف</th>
+<th class="text-center" >عنوان الاطروحة</th>
+<th class="text-center" style="width:5%;" >العمليات</th>
+</tr>
+</thead>';
+    $output .= '<tbody>';
+        foreach ($getData as $data)
+        {
+            $output .= "<tr><td>".$data['collegeId']."</td>
+            <td>".$data['SName1']." ".$data['SName2']." ".$data['SName3']." ".$data['SName4']." ".$data['SName5']."</td>
+            <td>".$data['VName1']." ".$data['VName2']." ".$data['VName3']." ".$data['VName4']." ".$data['VName5']."</td>
+            <td>".$data['RTitel']."</td>
+            <td>
+            <a class='btn btn-info' href=".$data['Researchurl']." >تنزيل ملف الاطروحة</a>
+            </td>
+            <tr>
+            ";
+        }
+            if (isset($_GET['file'])) {
+            $file = $_GET['file'];
+            if (file_exists($file) && is_readable($file) && preg_match('/\.pdf$/',$file)) {
+            header('Content-Type: application/pdf');
+            header("Content-Disposition: attachment; filename=\"$file\"");
+            readfile($file);
+             }
+         }
+        ?>
+    $output .= '</tbody>';
+$output .= '</table>';
+if(!empty($showpagination))
+{
+	$output .= '<ul class="pagination">'.$showpagination.'</ul>';
+}
+echo $output;
+die();
+?>
--- Library/index.php
+++ Library/index.php
@@ -10,21 +10,177 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>اهلا بمدير المكتبة</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>جامعة الموصل</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="../plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="../plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="../plugins/summernote/summernote-bs4.css">
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.2.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../dist/css/custom.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style type="text/css">
+    #overlay {background-color: rgba(0, 0, 0, 0.6);z-index: 999;position: absolute;left: 0;top: 0;width: 100%;height: 100%;display: none;}
+    #overlay div {position:absolute;left:50%;top:50%;margin-top:-32px;margin-left:-32px;}
+    .nav-link{font-size: 20px;}
     </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(document).on("click", ".page", function(){
+                $.ajax({
+                        url: "getresult.php",
+                        type: "GET",
+                        data:  {page:$(this).attr("data-page")},
+                        beforeSend: function(){$("#overlay").show();},
+                        success: function(data)
+                        {
+                            $("#pagination-result").html(data);
+                            setInterval(function() {$("#overlay").hide(); },500);
+                        },
+                        error: function() 
+                        {}          
+                   });
+            });
+        });
+    </script>
 </head>
-<body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-    <p>
-        <a href="../reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="../logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
-    <p><?php echo($_SESSION["userrole"]);?></p>
+<body >
+<div class="wrapper">
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark mb-0 " style="border-radius:0px;" >
+    <ul class="navbar-nav">
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index3.html" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+      <a href="#" class="nav-link">
+      <?php echo $_SESSION['username']; ?>
+      </a></li>
+    </ul>
+    <ul class="navbar-nav mr-auto-navbav">
+    <a class="nav-link" href="../logout.php">تسجيل الخروج</a>
+    </ul>
+  </nav>
+  <div class="content-wrapper mx-0">
+  
+    <section class="content">
+      <div class="container-fluid">
+      <div class="card  my-4">
+      <br>
+        <div class="card mx-4">
+              <div class="card-header" >
+                <div class=" row align-items-start mr-2">
+               <div  class="card-title"><h4>جدول الاطاريح</h4></div> 
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <?php
+    require_once("dbclass.php");
+    require_once("pagination.class.php");
+    $dbConnection  = new Connection();
+    $perPage       = new sbpagination();
+    $sqlquery      = "SELECT * from studentforma ";
+    $query         = $sqlquery."limit 0," . $perPage->perpage; 
+    $getData       = $dbConnection->runQuery($query);
+    $rowcount      = $dbConnection->numRows($sqlquery);
+    $showpagination = $perPage->getAllPageLinks($rowcount);
+    ?>
+ <div class="card-body mx-4">
+            <div id="pagination-result">
+            <table class="table table-bordered">
+                  <thead>                  
+                    <tr class='bg-dark'>
+                      <th class='text-center'>الكلية</th>
+                      <th class='text-center' >اسم الطالب</th>
+                      <th class='text-center' >المشرف</th>
+                      <th class='text-center' >عنوان الاطروحة</th>
+                      <th class='text-center' style="width:5%;" >العمليات</th>
+                    </tr>
+                  </thead>
+                    <tbody>
+                        <?php
+                        foreach ($getData as $data)
+                        {
+                           
+                            echo "<tr><td>".$data["collegeId"]."</td>
+                            <td>".$data['SName1']." ".$data['SName2']." ".$data['SName3']." ".$data['SName4']." ".$data['SName5']."</td>
+                            <td>".$data['VName1']." ".$data['VName2']." ".$data['VName3']." ".$data['VName4']." ".$data['VName5']."</td>
+                            <td>".$data['RTitel']."</td>
+                            <td>
+                            <a class='btn btn-info' href=".$data['Researchurl']." >تنزيل ملف الاطروحة</a>
+                            </td>
+                            <tr>
+                            ";
+                        }
+                            if (isset($_GET['file'])) {
+                            $file = $_GET['file'];
+                            if (file_exists($file) && is_readable($file) && preg_match('/\.pdf$/',$file)) {
+                            header('Content-Type: application/pdf');
+                            header("Content-Disposition: attachment; filename=\"$file\"");
+                            readfile($file);
+                             }
+                         }
+                        ?>
+                    </tbody>
+                </table>
+                <?php
+                if(!empty($showpagination))
+                {
+                    ?>
+                    <ul class="pagination">
+                        <?php echo $showpagination; ?>
+                    </ul>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+        </div>
+        <br>
+        </div>
+        </div>
+    </section>
+  </div>
+  <footer class="main-footer mx-0 px-4">
+  <b>Version</b> 1.0
+    <div class="float-right d-none d-sm-inline-block">
+    <strong>Copyright &copy; 2020-2021 <a href="https://uomosul.edu.iq">University of Mosul</a></strong>
+    All rights reserved
+    </div>
+  </footer>
+  <aside class="control-sidebar control-sidebar-dark">
+  </aside>
+</div>
+<script src="plugins/jquery/jquery.min.js"></script>
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<script src="https://cdn.rtlcss.com/bootstrap/v4.2.1/js/bootstrap.min.js"></script>
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../plugins/chart.js/Chart.min.js"></script>
+<script src="../plugins/sparklines/sparkline.js"></script>
+<script src="../plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="../plugins/moment/moment.min.js"></script>
+<script src="../plugins/daterangepicker/daterangepicker.js"></script>
+<script src="../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="../plugins/summernote/summernote-bs4.min.js"></script>
+<script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="../dist/js/adminlte.js"></script>
+<script src="../dist/js/demo.js"></script>
 </body>
 </html>
